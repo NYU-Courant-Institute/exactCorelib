c52f1e02c059a8f1a998daf669bf361513186f06
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2006-03-01 03:44:48 +0000

    Zilin:
    git-svn-id: https://subversive.cims.nyu.edu/exact@13 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/inc/CORE/Gmpq.h
+++ trunk/inc/CORE/Gmpq.h
@@ -19,7 +19,7 @@
  * WWW URL: http://cs.nyu.edu/exact/core
  * Email: exact@cs.nyu.edu
  *
- * $Id: Gmpq.h,v 1.1 2006-02-28 18:00:15 exact Exp $
+ * $Id: Gmpq.h,v 1.2 2006-03-01 03:44:48 exact Exp $
  ***************************************************************************/
 #ifndef __GMPQ_H__
 #define __GMPQ_H__
@@ -40,15 +40,8 @@ class Gmpq
   : public RcRepImpl<Gmpq>
 #endif
 {
-private:
-  mpq_t m_mp;
 public:
-  //internal structure accessors
-  const mpq_t& mp() const { return m_mp; }
-  mpq_t& mp() { return m_mp; }
-
-public:
-  /// \name constructors and destructor
+  /// \name constructors, destructor and assignment operator
   //@{
   /// default constructor
   Gmpq()
@@ -56,6 +49,9 @@ public:
   /// copy constructor
   Gmpq(const Gmpq& rhs)
   { mpq_init(m_mp); mpq_set(m_mp, rhs.m_mp); }
+  /// destructor
+  ~Gmpq()
+  { mpq_clear(m_mp); }
 
   /// constructor for <tt>long</tt>
   Gmpq(long i)
@@ -63,19 +59,15 @@ public:
   /// constructor for <tt>unsigned long</tt>
   Gmpq(unsigned long i)
   { mpq_init(m_mp); mpq_set_ui(m_mp, i, 1UL); }
-
   /// constructor for <tt>double</tt>
   Gmpq(double i)
   { mpq_init(m_mp); mpq_set_d(m_mp, i); }
-
   /// constructor for <tt>char*</tt> (no implicit conversion)
   explicit Gmpq(const char* str)
   { mpq_init(m_mp); mpq_set_str(m_mp, str, 0); mpq_canonicalize(m_mp); }
-
   /// constructor for <tt>mpz_t</tt>
   explicit Gmpq(const mpz_t& z)
   { mpq_init(m_mp); mpq_set_z(m_mp, z); }
-
   /// constructor for <tt>mpz_t, mpz_t</tt>
   explicit Gmpq(const mpz_t& num, const mpz_t& den) {
     mpq_init(m_mp); 
@@ -83,68 +75,59 @@ public:
     mpq_canonicalize(m_mp);
   }
 
-  /// destructor
-  ~Gmpq()
-  { mpq_clear(m_mp); }
-  //@}
-  
-  /// \name assignment operator
-  //@{
   /// assignment operator for <tt>Gmpq</tt>
   Gmpq& operator=(const Gmpq& rhs) {
     if (this != &rhs)  mpq_set(m_mp, rhs.m_mp); 
     return *this;
   }
   //@}
+public:
+  //internal structure accessors
+  const mpq_t& mp() const { return m_mp; }
+  mpq_t& mp() { return m_mp; }
+private:
+  mpq_t m_mp;
 };
 
 #ifndef CORE_DISABLE_REFCOUNTING
 /// \class RcGmpq Gmpq.h
-/// \brief RcGmpq is a wrapper class of <tt>Gmpq</tt> with referecen counting
+/// \brief RcGmpq is a wrapper class of <tt>Gmpq</tt> with reference counting
 class RcGmpq : public RcImpl<Gmpq> {
   typedef RcImpl<Gmpq> base_cls;
 public:
-  //internal structure accessors
-  const mpq_t& mp() const { return ((const Gmpq*)_rep)->mp(); }
-  mpq_t& mp() { return _rep->mp(); }
-
-public:
-  /// \name constructors and destructor
+  /// \name constructors, destructor and assignment operator
   //@{
   /// default constructor
   RcGmpq() : base_cls(new Gmpq()) {}
   /// copy constructor
   RcGmpq(const RcGmpq& rhs) : base_cls(rhs._rep) { _rep->inc_rc(); }
+  /// destructor
+  ~RcGmpq() { _rep->dec_rc(); }
 
   /// constructor for <tt>long</tt>
   RcGmpq(long i) : base_cls(new Gmpq(i)) {}
   /// constructor for <tt>unsigned long</tt>
   RcGmpq(unsigned long i) : base_cls(new Gmpq(i)) {}
-
   /// constructor for <tt>double</tt>
   RcGmpq(double i) : base_cls(new Gmpq(i)) {}
-
   /// constructor for <tt>char*</tt> (no implicit conversion)
   explicit RcGmpq(const char* str) : base_cls(new Gmpq(str)) {}
-
   /// constructor for <tt>mpz_t</tt>
   explicit RcGmpq(const mpz_t& z) : base_cls(new Gmpq(z)) {}
-
   /// constructor for <tt>mpz_t, mpz_t</tt>
   explicit RcGmpq(const mpz_t& num, const mpz_t& den) 
     : base_cls(new Gmpq(num, den)) {}
 
-  /// destructor
-  ~RcGmpq() { _rep->dec_rc(); }
-  //@}
-  
-  /// \name assignment operator
-  //@{
   /// assignment operator for <tt>RcGmpq</tt>
   RcGmpq& operator=(const RcGmpq& rhs) {
     if (this != &rhs) { _rep->dec_rc(); _rep = rhs._rep; _rep->inc_rc(); } 
     return *this;
   }
+  //@}
+public:
+  //internal structure accessors
+  const mpq_t& mp() const { return ((const Gmpq*)_rep)->mp(); }
+  mpq_t& mp() { return _rep->mp(); }
 };
 #endif
 
