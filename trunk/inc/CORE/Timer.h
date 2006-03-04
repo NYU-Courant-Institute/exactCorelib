998fa5cc4b3c0569f69dc6dd59592105a7dec8a4
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2006-03-04 05:08:48 +0000

    Zilin
    git-svn-id: https://subversive.cims.nyu.edu/exact@40 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/inc/CORE/Timer.h
+++ trunk/inc/CORE/Timer.h
@@ -19,11 +19,12 @@
  * WWW URL: http://cs.nyu.edu/exact/core
  * Email: exact@cs.nyu.edu
  *
- * $Id: Timer.h,v 1.4 2006-03-03 17:19:58 exact Exp $
+ * $Id: Timer.h,v 1.5 2006-03-04 05:08:48 exact Exp $
  ***************************************************************************/
 #ifndef __CORE_TIMER_H__
-#define __CORE_IIMER_H__
+#define __CORE_TIMER_H__
 
+#include <CORE/Config.h>
 #include <ctime>
 
 CORE_BEGIN_NAMESPACE
