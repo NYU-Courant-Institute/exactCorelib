13c31ef6967183946440387b2b3904bbf98efa91
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2006-08-09 09:40:22 +0000

    jihun:compute exact degree of expression
    git-svn-id: https://subversive.cims.nyu.edu/exact@149 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/inc/CORE/RootBounds.h
+++ trunk/inc/CORE/RootBounds.h
@@ -19,7 +19,7 @@
  * WWW URL: http://cs.nyu.edu/exact/core
  * Email: exact@cs.nyu.edu
  *
- * $Id: RootBounds.h,v 1.5 2006-08-07 14:03:32 exact Exp $
+ * $Id: RootBounds.h,v 1.6 2006-08-09 09:40:22 exact Exp $
  ***************************************************************************/
 #ifndef __CORE_ROOTBOUNDS_H__
 #define __CORE_ROOTBOUNDS_H__
@@ -49,46 +49,69 @@ public:
 /// BFMSS Root Bound
 template <typename Kernel = BigFloat>
 class BfmssRootBd {
-  unsigned long self_d_e;
-  unsigned int m_ref;
-  unsigned long d_e;
-  unsigned long u_e;
-  unsigned long l_e;
   typedef BfmssRootBd thisClass;
+  typedef thisClass* id_rootbd_t;
   typedef typename Kernel::ZT ZT;
   typedef typename Kernel::QT QT;
   typedef typename Kernel::FT FT;
+  // 8/9/2006 jihun: now we can guarantee that degree is computed exactly for the node who wants rootbound
+  // TODO: u_d_e easily overflows unsigned long range. we need to keep ln(u_d_e) instead.
+  id_rootbd_t r_id;      // id of rootbd node which needs exact degree value
+  unsigned long m_visit; // count of visit 
+  unsigned long u_d_e;   // upper bound on the degree of expression
+  unsigned long e_d_e;   // exact value of the degree of expression 
+  unsigned long r_d_e;   // temporary value of the degree of root expression, used to check sharing nodes
+  unsigned long u_e;
+  unsigned long l_e;
 public:
-  BfmssRootBd() : self_d_e(1), m_ref(1) {}
-#ifdef CORE_DEBUG_ROOTBOUND
+  BfmssRootBd() : r_id(0), u_d_e(1), e_d_e(0), r_d_e(1) {}
+//#ifdef CORE_DEBUG_ROOTBOUND
   void dump() const 
-  { std::cerr<<"[d_e,u_e,l_e,bound]="<<d_e<<","<<u_e<<","<<l_e<<","<< get_bound() <<std::endl; }
-#endif
-  unsigned long get_self_degree()
-  { return self_d_e; }
-  void init_ref()
-  { m_ref=0; }
-  void inc_ref()
-  { ++m_ref; }
-  bool is_used()
-  { return m_ref > 1; }
+  //{ std::cout<<"[d_e,u_e,l_e,bound]="<<d_e<<","<<u_e<<","<<l_e<<","<< get_bound() <<std::endl; }
+  { std::cout<<"[u_d_e,e_d_e,r_d_e]="<<u_d_e<<","<<e_d_e<<","<<get_r_deg()<<std::endl; }
+//#endif
+protected:
+  unsigned long get_u_deg() const {
+    if (is_exact()) return e_d_e;
+    else return u_d_e;
+  }
+  unsigned long get_e_deg() const {
+    return e_d_e;
+  }
+  unsigned long get_r_deg() const {
+    if (m_visit > 1) return 1;
+    else return r_d_e;
+  }
+  void compute_deg() {
+    if (u_d_e==r_d_e) e_d_e=r_d_e;
+    if (r_id==this) { u_d_e=r_d_e; e_d_e=r_d_e; }
+  }
+public:
+  const id_rootbd_t get_id() {
+    return (this);
+  }
+  void set_id (id_rootbd_t id) {
+    if (r_id == id) ++m_visit;
+    else { r_id = id;  m_visit=1; }
+  }    
+  bool is_exact() const
+  { return e_d_e > 0; }
   bool is_constructive() const
   { return true; }
   /// BFMSS root bound could be zero, so set it to be 1 in that case
-  unsigned long get_bound(unsigned long deg) const
-  { return std::max(l_e + (deg - 1) * u_e, 1UL); }
   unsigned long get_bound() const
-  { return std::max(l_e + (d_e - 1) * u_e, 1UL); }
+  { return std::max(l_e + (get_u_deg() - 1) * u_e, 1UL);
+  }
   void set(long value)
-  { u_e = ceillg(abs(value)); l_e = 0; d_e = 1; }
+  { u_e = ceillg(value); l_e = 0; }
   void set(unsigned long value)
-  { u_e = ceillg(value); l_e = 0; d_e = 1; }
+  { u_e = ceillg(value); l_e = 0; }
   void set(double value)
   { set(FT(value)); }
   void set(const ZT& value)
-  { u_e = abs(value).ceillg(); l_e = 0; d_e = 1; }
+  { u_e = value.ceillg(); l_e = 0; }
   void set(const QT& value)
-  { u_e = abs(value).num().ceillg(); l_e = abs(value).den().ceillg(); d_e = 1; }
+  { u_e = value.num().ceillg(); l_e = value.den().ceillg(); }
   void set(const FT& value) {
     ZT x; exp_t e = value.get_z_exp(x);
     if (e >= 0) { // convert to integer
@@ -103,8 +126,12 @@ public:
     else
       set(value.getRight());
   }
-  void neg(const thisClass& child)
-  { u_e = child.u_e; l_e = child.l_e; d_e = child.d_e; }
+  void neg(const thisClass& child) {
+    u_e = child.u_e; l_e = child.l_e;
+    u_d_e = child.get_u_deg();
+    r_d_e = child.get_r_deg(); 
+    e_d_e = child.get_e_deg(); 
+  }
   void root(thisClass& child, unsigned long k) {
     if (child.u_e >= child.l_e) {
       u_e = (child.u_e + (k-1)*child.l_e + (k-1)) / k;
@@ -113,26 +140,49 @@ public:
       u_e = child.u_e;
       l_e = ((k-1)*child.u_e + child.l_e + (k-1)) / k;
     }
-    d_e = k * child.d_e;
-    self_d_e = k;
+    u_d_e = k * child.get_u_deg();
+    r_d_e = k * child.get_r_deg();
+    e_d_e = 0;
+    compute_deg(); 
   }
   void addsub(thisClass& f, thisClass& s) {
     u_e = std::max(f.u_e + s.l_e, f.l_e + s.u_e) + 1; 
     l_e = f.l_e + s.l_e;
-    if (&f==&s) d_e = f.d_e;
-    else d_e = f.d_e * s.d_e;
+    if (&f!=&s) {
+      u_d_e = f.get_u_deg() * s.get_u_deg();
+      r_d_e = f.get_r_deg() * s.get_r_deg();
+    } else {
+      u_d_e = f.get_u_deg();
+      r_d_e = f.get_u_deg();
+    }
+    e_d_e = 0;
+    compute_deg(); 
   }
   void mul(thisClass& f,thisClass& s) {
     u_e = f.u_e + s.u_e;
     l_e = f.l_e + s.l_e;
-    if (&f==&s) d_e = f.d_e;
-    else d_e = f.d_e * s.d_e;
+    if (&f!=&s) {
+      u_d_e = f.get_u_deg() * s.get_u_deg();
+      r_d_e = f.get_r_deg() * s.get_r_deg();
+    } else {
+      u_d_e = f.get_u_deg();
+      r_d_e = f.get_u_deg();
+    }
+    e_d_e = 0;
+    compute_deg(); 
   }
   void div(thisClass& f, thisClass& s) { 
     u_e = f.u_e + s.l_e;
     l_e = f.l_e + s.u_e;
-    if (&f==&s) d_e = f.d_e;
-    else d_e = f.d_e * s.d_e;
+    if (&f!=&s) {
+      u_d_e = f.get_u_deg() * s.get_u_deg();
+      r_d_e = f.get_r_deg() * s.get_r_deg();
+    } else {
+      u_d_e = f.get_u_deg();
+      r_d_e = f.get_u_deg();
+    }
+    e_d_e = 0;
+    compute_deg(); 
   }
 };
 
