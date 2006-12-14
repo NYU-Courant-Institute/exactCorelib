485e597ad78299bfcf767855a20109d1f6b4a693
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2006-12-14 22:07:51 +0000

    Chee/Jihun
    git-svn-id: https://subversive.cims.nyu.edu/exact@194 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/src/MpfrIO.cpp
+++ trunk/src/MpfrIO.cpp
@@ -19,7 +19,7 @@
  * WWW URL: http://cs.nyu.edu/exact/core
  * Email: exact@cs.nyu.edu
  *
- * $Id: MpfrIO.cpp,v 1.8 2006-12-14 20:01:55 exact Exp $
+ * $Id: MpfrIO.cpp,v 1.9 2006-12-14 22:07:51 exact Exp $
  ***************************************************************************/
 #include <mpfr.h>
 #include <string>
@@ -141,8 +141,9 @@ std::string mpfr2str(
     result.assign(str, len+first);
     if (exp > 0) {  
       // BUG fixed: if ((unsigned long)exp > len-first) (12/14/06)
+      //   result.append(exp - len+first, '0');
       if ((unsigned long)exp > len) // integer need padding 0
-        result.append(exp - len+first, '0');
+        result.append(exp - len, '0');
       else if ((unsigned long)exp < len) // float point value
         result.insert(first + exp, ".");
     } else if (exp < 0) {
