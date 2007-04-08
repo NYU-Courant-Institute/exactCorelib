e96ba68f28538942be7cf083fabdfcdb2f39ea04
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2007-04-08 02:01:43 +0000

    jihun
    git-svn-id: https://subversive.cims.nyu.edu/exact@262 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/testIO/testBigFloat.cpp
+++ trunk/progs/testIO/testBigFloat.cpp
@@ -11,7 +11,7 @@
    Author: Jihun and Chee Yap
 
    Since Core Library 2.0
-   $Id: testBigFloat.cpp,v 1.1 2007-03-07 20:26:25 exact Exp $
+   $Id: testBigFloat.cpp,v 1.2 2007-04-08 02:01:43 exact Exp $
  ************************************************ */  
 
 #ifndef CORE_LEVEL
@@ -23,27 +23,6 @@
 using namespace std;
 
 using namespace CORE;
-
-//////////////////////////////////////////////////
-// test
-//////////////////////////////////////////////////
-
-/*
-template <class T1, class T2>
-void test(T1 ans, T2 unknown, string msg1 = "", msg2 = "") {
- if (ans != unknown) {
-	 cout << "ERROR!!! " << msg1 << endl;
-	 cout << "  answer : " << ans << endl;
-	 cout << "  unknown : " << unknown << endl;
- }
- else if (coretest_verbose) {
-	 cout << "CORRECT!!! " << msg2 << endl;
-	 cout << "  answer : " << ans << endl;
-	 cout << "  unknown : " << unknown << endl;
- }
- coretest_error = true;
-}
-*/	
 
 //////////////////////////////////////////////////
 // main
@@ -71,25 +50,27 @@ int main( int argc, char *argv[] ) {
   
   // get mantissa:
   f = BigFloat("31459");
-  BigInt z;
-  f.get_z_exp(z);
+  BigInt z; int exp;
+  exp = f.get_z_exp(z);
+  
+  if(exp >= 0)
+    z *= pow(BigInt(2), exp);
+  else
+    z /= pow(BigInt(2), -exp);
+
   core_test("31459", z.get_str(), "f.get_z_exp() is wrong");
   //
   
   // BigFloat2 string input:
   BigFloat2 f2("0.0007891", 10); 
-  core_test("0.0007891", f2.get_str(), "BigFloat string input is wrong");
+  core_test(".0007891", f2.get_str(7), "BigFloat string input is wrong");
   //
   
   f = "0.0625";
   f2 = BigFloat2(f);
   core_test(".0625", f2.get_str(), "BigFloat2.get_str is wrong");
 
-  // BigFloat2 string input:
-  core_test(".0007891", f2.get_str(4), "BigFloat2 string input is wrong");
-  //
-
-  if (coretest_error = false)
+  if (coretest_error == false)
     cout << "CORRECT!!! all test are passed" << endl;
 
   return 0;
