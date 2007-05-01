85b535009a04f343b111c2645631fd3a7f40f595
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2007-05-01 16:38:10 +0000

    Chee
    git-svn-id: https://subversive.cims.nyu.edu/exact@280 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/generic/sample.cpp
+++ trunk/progs/generic/sample.cpp
@@ -3,7 +3,7 @@
   purpose:
 	a simple sample CORE program
   CORE Library 
-  $Id: sample.cpp,v 1.2 2006-04-03 20:39:39 exact Exp $
+  $Id: sample.cpp,v 1.3 2007-05-01 16:38:10 exact Exp $
 ************************************************************/
  
 #ifndef CORE_LEVEL
@@ -13,63 +13,69 @@
 // Important: place all standard header files BEFORE "CORE.h"
 #include "CORE.h"
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
 
   double x = 1.44;	// Inexact input; C++ literal input precision
 
-  setDefaultInputDigits(20);  // This should be better than C++ precision
+  setDefaultInputDigits(20);  // This is more than usual C++ precision
   double y = "1.44";	// Use of string constructor 
 
   setDefaultInputDigits(CORE_INFTY); // force exact input 
   double z = "1.44";	// z is represented as a rational number
   
-  int p = getDefaultOutputDigits(); // p = defOutputDigits
-  std::cout << "    C++'s Default Printout Digits is " << p << std::endl;
-  std::cout << "    CORE Default Printout Digits is " << getDefaultOutputDigits() 
-	<< std::endl;
+  int p = getDefaultOutputDigits(); // p = defOutputDigits (p=6)
+  cout << "    C++'s Default Printout Digits is " << p << endl;
+  cout << "    CORE Default Printout Digits is " << getDefaultOutputDigits() 
+	<< endl;
 
-  std::cout << "    x = 1.44;  // standard C++ literal input" << std::endl;
-  std::cout << "Printout of x: " << x << std::endl;
-  std::cout << "      sqrt(x): " << sqrt(x) << std::endl;
-  std::cout << "==================================================" << std::endl;
+  cout << "    x = 1.44;  // standard C++ literal input" << endl;
+  cout << "Printout of x: " << x << endl;
+  cout << "      sqrt(x): " << sqrt(x) << endl;
+  cout << "==================================================" << endl;
 
-  std::cout << std::setprecision(6) ;
-  std::cout << "    y = \"1.44\". // string literal; defInputDigits = 20\n";
-  std::cout << "    Set CORE Printout Digits to 6 " << std::endl;
-  std::cout << "Printout of y: " << y << std::endl;
-  std::cout << "      sqrt(y): " << sqrt(y) << std::endl;
-  std::cout << "==================================================" << std::endl;
+  cout << setprecision(6) ;
+  cout << "    y = \"1.44\". // string literal; defInputDigits = 20\n";
+  cout << "    Set CORE Printout Digits to 6 " << endl;
+  cout << "Printout of y: " << y << endl;
+  cout << "      sqrt(y): " << sqrt(y) << endl;
+  cout << "==================================================" << endl;
 
-  std::cout << std::setprecision(6) ;
-  std::cout << "    z = \"1.44\". // string literal; defInputDigits = Infinite\n";
-  std::cout << "    Set CORE Printout Digits to 6 " << std::endl;
-  std::cout << "Printout of z: " << z << std::endl;
-  std::cout << "      sqrt(z): " << sqrt(z) << std::endl;
-  std::cout << "==================================================" << std::endl;
-  std::cout << "    Above, x, y and z look the same."
+  cout << setprecision(6) ;
+  cout << "    z = \"1.44\". // string literal; defInputDigits = Infinite\n";
+  cout << "    Set CORE Printout Digits to 6 " << endl;
+  cout << "Printout of z: " << z << endl;
+  cout << "      sqrt(z): " << sqrt(z) << endl;
+  cout << "==================================================" << endl;
+  cout << "    Above, x, y and z look the same."
 	<< "\n    To see that they are not the same, we force more"
-	<< "\n    precision in evaluation and in output:" << std::endl;
+	<< "\n    precision in evaluation and in output:" << endl;
 
-  std::cout << "---Set defRelPrec 133 bits = 40 digits"
-	<< "\n ---Set defPrintDigits to 40 " << std::endl;
+  cout << " ---Set defRelPrec 133 bits = 40 digits"
+	<< "\n ---Set defPrintDigits to 40 " << endl;
   defRelPrec = 133; // this gives 40 digits of precision
   setDefaultOutputDigits(40);
 
-  std::cout << "sqrt(x) = " << sqrt(x) << std::endl;
-  std::cout << "sqrt(y) = " << sqrt(y) << std::endl;
-  std::cout << "sqrt(z) = " << sqrt(z) << std::endl;
-  std::cout << "==================================================" << std::endl;
+  cout << "x = " << x << endl;
+  cout << "y = " << y << endl;
+  cout << "z = " << z << endl;
+  cout << "sqrt(x) = " << sqrt(x) << endl;
+  cout << "sqrt(y) = " << sqrt(y) << endl;
+  cout << "sqrt(z) = " << sqrt(z) << endl;
+  cout << "==================================================" << endl;
 
-  std::cout << "    Even though sqrt(z) looks like 1.2, the only way to"
-	<< "\n   verify this is to do a comparison with 1.2" << std::endl;
+  cout << "   Even though sqrt(z) looks like 1.2, the only way to"
+	<< "\n   verify this is to do a comparison with 1.2:" << endl;
 
+  setDefaultInputDigits(CORE_INFTY); // force exact input 
   double sqrtz = "1.2";  // exact input
 
   if (sqrt(z) == sqrtz)
-	std::cout << "CORRECT!  sqrt(z) = 1.2 exactly" << std::endl;
+	cout << "CORRECT!  sqrt(z) = 1.2 exactly" << endl;
   else
-	std::cout << "ERROR!  sqrt(z) != 1.2 " << std::endl;
-  std::cout << "==================================================" << std::endl;
+	cout << "ERROR!  sqrt(z) != 1.2 " << endl;
+  cout << "==================================================" << endl;
 
   return (0);
 }
