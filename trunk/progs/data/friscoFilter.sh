9574d481a03b97b774135871eddaf9ecbe59d0b6
irina <irina@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2012-06-13 03:32:17 +0000

    script to filter FRISCO polynomial names into real vs complex
    git-svn-id: https://subversive.cims.nyu.edu/exact@1064 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

---
+++ trunk/progs/data/friscoFilter.sh
@@ -0,0 +1,35 @@
+# !/bin/sh
+
+# echo "                                           START"
+
+# check command line argument
+if [ $1 = r ]; then
+    echo "Printing names of real polynomials" $1
+    decide=r
+elif  [ $1 = c ]; then
+    echo "Printing names of complex polynomials" $1
+    decide=c
+else
+    echo "Usage " $0 "[c|r]  defaulting to r"
+    decide=r
+fi
+
+
+# process every FRISCO polynomial in the current directory
+for x in ./frisco/*pol  ;
+   { 
+    # echo "Processing polynomial" $x "..."; 
+	
+    # Select first few lines of the polynomial file
+    # ignore those that are comments
+    # look for "c" in complex and for "r" in real polynomials
+    # head -5 $x | grep -v "!" | grep -c "c" 
+
+    if [[ `head -5 $x | grep -v "!" | grep -c $decide` -ne 0 ]]; then
+           # real polynomial
+           echo $x
+    fi
+   } 
+
+
+# echo "                                           END"
