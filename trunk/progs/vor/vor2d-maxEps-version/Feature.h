b37ea24cd04322f9e843231e4b2050b0707b261d
exact <exact@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2012-06-27 03:30:00 +0000

    yap-macOS:svncom
    git-svn-id: https://subversive.cims.nyu.edu/exact@1164 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/vor/vor2d-maxEps-version/Feature.h
+++ trunk/progs/vor/vor2d-maxEps-version/Feature.h
@@ -1,7 +1,18 @@
-
+/*
+ * file: Feature.h
+ *
+ * 	This is the main file that controls the CORE LEVEL...
+ *
+ * 	We generally prefer to default to LEVEL 1
+ * 	as it could be slow for LEVEL 2 or 3.
+ *
+ * 	Author: Jyh-Ming Lien
+ * 	Since Core 2.1
+ *
+ * ***************************************************/
 #pragma once
 
-#define CORE_LEVEL 3
+#define CORE_LEVEL 1
 #include "CORE.h"
 
 class Set; //defined in "UnionFind.h"
