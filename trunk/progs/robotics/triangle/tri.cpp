c63b25362b943e8ddd08127f82451f3e4d2e42df
yjc <yjc@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2013-09-27 14:43:58 +0000

    git-svn-id: https://subversive.cims.nyu.edu/exact@2331 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/robotics/triangle/tri.cpp
+++ trunk/progs/robotics/triangle/tri.cpp
@@ -56,8 +56,10 @@
 #include "GL/glui.h"
 #endif
 #ifdef __linux__
+#include <GL/glew.h>
 #include <GL/glut.h>
-#include "GL/glui.h"
+#include <GL/glui.h>
+// #include "GL/glui.h"
 #endif
 #ifdef _WIN32
 #include <gl/glew.h>
