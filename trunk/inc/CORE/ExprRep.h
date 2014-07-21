eae9189abcb84b097aaa2fe264101e54f5aa598b
yap <yap@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2014-07-21 21:55:54 +0000

    yap-macOS:svncom
    git-svn-id: https://subversive.cims.nyu.edu/exact@3378 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/inc/CORE/ExprRep.h
+++ trunk/inc/CORE/ExprRep.h
@@ -2035,7 +2035,7 @@ class MulRepT : public BinaryOpRepT<RootBd, Filter, Kernel> {
   typedef ExprRepT<RootBd, Filter, Kernel> ExprRep;
   typedef BinaryOpRepT<RootBd, Filter, Kernel> BinaryOpRep;
   typedef RootBd* id_rootbd_t;
-  using BinaryOpRep::check_exact;	// This line added Apr24'2013 (Chee)
+  //using BinaryOpRep::check_exact;	// This line added Apr24'2013 (Chee)
   using BinaryOpRep::first; 
   using BinaryOpRep::second; 
   using BinaryOpRep::check_exact;
@@ -2094,7 +2094,7 @@ class DivRepT : public BinaryOpRepT<RootBd, Filter, Kernel> {
   using BinaryOpRep::check_exact;	// This line added Apr24'2013 (Chee)
   using BinaryOpRep::first; 
   using BinaryOpRep::second; 
-  using BinaryOpRep::check_exact;	// Chee: April 2013
+  //using BinaryOpRep::check_exact;	// Chee: April 2013
   using ExprRep::filter;
   using ExprRep::rootBd;
   using ExprRep::sign;
