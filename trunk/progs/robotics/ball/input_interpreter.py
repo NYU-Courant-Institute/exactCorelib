e53569fd1d9f348504f382e496aab54746316dec
ziyao <ziyao@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2014-10-05 16:02:07 +0000

    Multiple bodys input
    git-svn-id: https://subversive.cims.nyu.edu/exact@3509 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/robotics/ball/input_interpreter.py
+++ trunk/progs/robotics/ball/input_interpreter.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def readNextSetOfLines(lines):
     """Given a set of lines beginning with a number n, return a pair with n and the list of the next n lines, and remove the content of the tuple from the input"""
     n = int(lines[0])
@@ -9,7 +8,9 @@ def readNextSetOfLines(lines):
     return (n, nLines)
 
 class Triple(tuple):
+    """Triple class"""
     def __new__(cls, *args):
+        # TODO: Check if triple
         return tuple.__new__(cls, args)
     def __add__(self, other):
         return Triple(*([sum(x) for x in zip(self, other)]))
@@ -20,8 +21,9 @@ input = open('output-tmp.txt', 'r')
 output = open('output-tmp-py.txt', 'w')
 lines = [x.strip(' \t\n\r') for x in input.readlines()]
 (numPoints, pointsLines) = readNextSetOfLines(lines)
-(numFaces, facesLines) = readNextSetOfLines(lines)
-
+listFaces = []
+while int(lines[0]) != 0:
+    listFaces.append(readNextSetOfLines(lines))
 
 points2Indices = {}
 points = []
@@ -39,36 +41,39 @@ for i in range(numPoints):
             p = p + points[points2Indices[point[j]] - 1]
     points.append(p)
 
-faces = []
-for i in range(numFaces):
-    face = facesLines[i].split()
-    faceType = int(face[0])
-    face = face[1:]
-    for j in range(len(face)):
-        if not face[j].isdigit():
-            face[j] = str(points2Indices[face[j]])
-    if ((faceType == 0 or faceType == 1) and len(face) == 6) or faceType == 3:
-        p = (face[-3], face[-2], face[-1])
-        face = face[:-3]
-        for j in range(len(face)):
-            basePoint = points[int(face[j]) - 1]
-            newPoint = p + basePoint
-            points.append(newPoint)
-            face[j] = str(len(points))
-    if faceType == 1:
-        # f = str(faceType) + " " + " ".join(face) + "\n"
-        faces.append("0 " + " ".join([str(x) for x in face]) + "\n")
-        base = points[int(face[0]) - 1]
-        p = points[int(face[1]) - 1] + points[int(face[2]) - 1] + Triple(-base[0], -base[1], -base[2])
-        points.append(p)
-        faces.append("0 " + str(face[1]) + " " + str(len(points)) + " " + str(face[2]) + "\n")
-    elif faceType == 2 or faceType == 3:
-        base = face[0]
+for j in range(len(listFaces)):
+    numFaces = listFaces[j][0]
+    facesLines = listFaces[j][1]
+    faces = []
+    for i in range(numFaces):
+        face = facesLines[i].split()
+        faceType = int(face[0])
         face = face[1:]
-        for j in range(len(face) - 1):
-            faces.append("0 " + str(base) + " " + str(face[j]) + " " + str(face[j + 1]) + "\n")
-    else:
-        faces.append(str(faceType) + " " + " ".join(face) + "\n")
+        for j in range(len(face)):
+            if not face[j].isdigit():
+                face[j] = str(points2Indices[face[j]])
+        if ((faceType == 0 or faceType == 1) and len(face) == 6) or faceType == 3:
+            p = (face[-3], face[-2], face[-1])
+            face = face[:-3]
+            for j in range(len(face)):
+                basePoint = points[int(face[j]) - 1]
+                newPoint = p + basePoint
+                points.append(newPoint)
+                face[j] = str(len(points))
+        if faceType == 1:
+            # f = str(faceType) + " " + " ".join(face) + "\n"
+            faces.append("0 " + " ".join([str(x) for x in face]) + "\n")
+            base = points[int(face[0]) - 1]
+            p = points[int(face[1]) - 1] + points[int(face[2]) - 1] + Triple(-base[0], -base[1], -base[2])
+            points.append(p)
+            faces.append("0 " + str(face[1]) + " " + str(len(points)) + " " + str(face[2]) + "\n")
+        elif faceType == 2 or faceType == 3:
+            base = face[0]
+            face = face[1:]
+            for j in range(len(face) - 1):
+                faces.append("0 " + str(base) + " " + str(face[j]) + " " + str(face[j + 1]) + "\n")
+        else:
+            faces.append(str(faceType) + " " + " ".join(face) + "\n")
 
 output.write(str(len(points)) + "\n")
 output.write("\n".join([" ".join([str(j) for j in i]) for i in points]) + "\n")
