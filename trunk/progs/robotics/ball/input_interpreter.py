d966c5d1e37e5aa45cacc82eff8704fa3cd03f84
ziyao <ziyao@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2014-10-14 21:01:41 +0000

    Thick arbitrary polygons
    git-svn-id: https://subversive.cims.nyu.edu/exact@3528 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/robotics/ball/input_interpreter.py
+++ trunk/progs/robotics/ball/input_interpreter.py
@@ -48,9 +48,9 @@ def string2Triple(s, points2Indices, points):
     if len(s) == 1:
         # name
         if s[0].isdigit():
-            return points[int(s[0]) - 1]
+            return points[int(s[0])]
         else:
-            return points[points2Indices[s[0]] - 1]
+            return points[points2Indices[s[0]]]
     elif len(s) == 3:
         # coordinates
         return Triple(*map(int, s))
@@ -68,17 +68,18 @@ def reduceListOps(operands, operators, points2Indices, points):
     return p
 
 input = open('output-tmp.txt', 'r')
-os.remove('output-tmp-py.txt')
+try:
+    os.remove('output-tmp-py.txt')
+except OSError:
+    pass
 output = open('output-tmp-py.txt', 'w')
 lines = [x.strip(' \t\n\r') for x in input.readlines()]
 listFaces = []
-numPoints = 0
 pointsLines = []
 puts = []
 while lines[0] != "End":
-    if lines[0] == "Points":
+    if lines[0].startswith("Points"):
         (n, pl, name) = readNextSetOfLines(lines)
-        numPoints += n
         pointsLines += pl
     elif lines[0].startswith("Put"):
         puts.append(lines[0])
@@ -87,25 +88,34 @@ while lines[0] != "End":
         listFaces.append(readNextSetOfLines(lines))
 
 # Processing the explicitly defined input points:
-def parsePointsSection(pointsLines):
-    points2Indices = {}
-    points = []
-    for i in range(numPoints):
+def parsePointsSection(pointsLines, points, points2Indices, pointId = None):
+    if points:
+        if not pointId:
+            initId = max(points) + 1
+        elif pointId <= max(points):
+            raise Exception()
+    else:
+        if not pointId:
+            pointId = 1
+    for i in range(len(pointsLines)):
         line = pointsLines[i]
         if line[0] == '"':
             line = line[1:]
             lst = line.split('"')
             name = lst[0]
-            points2Indices[name] = i + 1
+            points2Indices[name] = pointId
             line = lst[1]
         (base, operands, operators) = splitLineByOperator(line)
         p = string2Triple(base, points2Indices, points)
         p += reduceListOps(operands, operators, points2Indices, points)
-        points.append(p)
+        points[pointId] = p
+        pointId += 1
     return (points2Indices, points)
 
 # Processing the explicitly defined input points:
-(points2Indices, points) = parsePointsSection(pointsLines)
+points2Indices = {}
+points = {}
+(points2Indices, points) = parsePointsSection(pointsLines, points, points2Indices)
 
 faces = []
 poly2faces = {}
@@ -128,21 +138,22 @@ for j in range(len(listFaces)):
                 face[j] = str(points2Indices[face[j]])
         if p != Triple(0, 0, 0):
             for j in range(len(face)):
-                basePoint = points[int(face[j]) - 1]
+                basePoint = points[int(face[j])]
                 newPoint = p + basePoint
-                points.append(newPoint)
-                face[j] = str(len(points))
+                points[max(points) + 1] = newPoint
+                face[j] = max(points)
         if faceType == 1:
             faces.append(face)
-            base = points[int(face[0]) - 1]
-            p = points[int(face[1]) - 1] + points[int(face[2]) - 1] + Triple(-base[0], -base[1], -base[2])
-            points.append(p)
-            faces.append([face[1], len(points), face[2]])
+            base = points[int(face[0])]
+            p = points[int(face[1])] + points[int(face[2])] + Triple(-base[0], -base[1], -base[2])
+            points[max(points) + 1] = p
+            faces.append([face[1], max(points), face[2]])
         elif faceType == 2 or faceType == 3:
             base = face[0]
             face = face[1:]
             for j in range(len(face) - 1):
                 faces.append([base, face[j], face[j + 1]])
+                print faces
         elif faceType == 4:
             faces.append(face[:])
             face.reverse()
@@ -152,13 +163,19 @@ for j in range(len(listFaces)):
             face.reverse()
             faces.append(face[:])
             face.reverse()
-            base = points[int(face[0]) - 1]
-            p = points[int(face[1]) - 1] + points[int(face[2]) - 1] + Triple(-base[0], -base[1], -base[2])
-            points.append(p)
-            otherFace = [face[1], len(points), face[2]]
+            base = points[int(face[0])]
+            p = points[int(face[1])] + points[int(face[2])] + Triple(-base[0], -base[1], -base[2])
+            points[max(points) + 1] = p
+            otherFace = [face[1], max(points), face[2]]
             faces.append(otherFace[:])
             otherFace.reverse()
             faces.append(otherFace[:])
+        elif faceType == 6:
+            base = face[0]
+            face = face[1:]
+            for j in range(len(face) - 1):
+                faces.append([base, face[j], face[j + 1]])
+                faces.append([face[j + 1], face[j], base])
         else:
             faces.append(face)
     end = len(faces)
@@ -172,15 +189,15 @@ for i in puts:
     for j in range(*facesRange):
         face = []
         for k in range(len(faces[j])):
-            basePoint = points[int(faces[j][k]) - 1]
+            basePoint = points[int(faces[j][k])]
             newPoint = p + basePoint
-            points.append(newPoint)
-            face.append(str(len(points)))
+            points[max(points) + 1] = newPoint
+            face.append(max(points))
         faces.append(face)
 
 faces = ['0 ' + ' '.join([str(j) for j in i]) + "\n" for i in faces]
-output.write(str(len(points)) + "\n")
-output.write("\n".join([' '.join([str(j) for j in i]) for i in points]) + "\n")
+output.write(str(max(points)) + "\n")
+output.write("\n".join([' '.join([str(j) for j in points[i]]) for i in points]) + "\n")
 output.write(str(len(faces)) + "\n")
 output.write(''.join(faces))
 output.write("0\n")
