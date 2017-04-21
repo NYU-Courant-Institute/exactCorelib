a19bff94ff059f31ae1613719e90551e252a268f
bo <bo@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2017-04-21 05:31:11 +0000

    git-svn-id: https://subversive.cims.nyu.edu/exact@6144 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/robotics/complex-qt/complex-qt.cpp
+++ trunk/progs/robotics/complex-qt/complex-qt.cpp
@@ -404,29 +404,29 @@ bool loadComplexRobot(string& robot_file, vector<Triangle>& compRoboTri) {
 
 int main(int argc, char* argv[]) {
 
-	if (argc >1) interactive = atoi(argv[1]);//interactive or no
-	if (argc >2) cfgName = argv[2];		//config file
-	if (argc >3) fileName = argv[3]		//enviroment file
-	if (argc >4) inputDir = argv[4]		//Path for input files
-	if (argc >5) robotDir = argv[5]		//Path for robot files
-	if (argc >6) robotName = argv[6]	//robot 
-	if (argc >7) alpha[0] = atof(argv[7])	// startx	
-	if (argc >8) alpha[1] = atof(argv[8])	// starty
-	if (argc >9) alpha[2] = atof(argv[9])	// startTheta
-	if (argc >10) beta[0] = atof(argv[10])	// goalx
-	if (argc >11) beta[1] = atof(argv[11])	// goaly
-	if (argc >12) beta[2] = atof(argv[12])	// goalTheta
-	if (argc >13) epsilon = atof(argv[13])	// resolution parameter
-	if (argc >14) R0 	= atof(argv[14])	// robot radius
-	if (argc > 15) boxWidth = atof(argv[15]);		// boxWidth
-	if (argc > 16) boxHeight = atof(argv[16]);	// boxHeight
-	if (argc > 17) windowPosX = atoi(argv[17]);	// window X pos
-	if (argc > 18) windowPosY = atoi(argv[18]);	// window Y pos
-	if (argc > 19) QType   = atoi(argv[19]); // PriorityQ Type(random or no)
-	if (argc > 20) seed   = atoi(argv[20]);	 // for random number generator
-	if (argc > 21) deltaX  = atof(argv[21]); // x-translation of input file
-	if (argc > 22) deltaY  = atof(argv[22]); // y-translation of input file
-	if (argc > 23) scale  = atof(argv[23]);		// scaling of input file
+    if (argc >1) interactive = atoi(argv[1]);//interactive or no
+    if (argc >2) cfgName = argv[2];		//config file
+    if (argc >3) fileName = argv[3];		//enviroment file
+    if (argc >4) inputDir = argv[4];		//Path for input files
+    if (argc >5) robotDir = argv[5];		//Path for robot files
+    if (argc >6) robotName = argv[6];	//robot
+    if (argc >7) alpha[0] = atof(argv[7]);	// startx
+    if (argc >8) alpha[1] = atof(argv[8]);	// starty
+    if (argc >9) alpha[2] = atof(argv[9]);	// startTheta
+    if (argc >10) beta[0] = atof(argv[10]);	// goalx
+    if (argc >11) beta[1] = atof(argv[11]);	// goaly
+    if (argc >12) beta[2] = atof(argv[12]);	// goalTheta
+    if (argc >13) epsilon = atof(argv[13]);	// resolution parameter
+    if (argc >14) R0 	= atof(argv[14]);	// robot radius
+    if (argc > 15) boxWidth = atof(argv[15]);		// boxWidth
+    if (argc > 16) boxHeight = atof(argv[16]);	// boxHeight
+    if (argc > 17) windowPosX = atoi(argv[17]);	// window X pos
+    if (argc > 18) windowPosY = atoi(argv[18]);	// window Y pos
+    if (argc > 19) QType   = atoi(argv[19]); // PriorityQ Type(random or no)
+    if (argc > 20) seed   = atoi(argv[20]);	 // for random number generator
+    if (argc > 21) deltaX  = atof(argv[21]); // x-translation of input file
+    if (argc > 22) deltaY  = atof(argv[22]); // y-translation of input file
+    if (argc > 23) scale  = atof(argv[23]);		// scaling of input file
 
 
     bool foundFiles = false;
