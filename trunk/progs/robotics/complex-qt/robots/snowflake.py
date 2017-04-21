ff4b160d1a987d36b6693efe9d8537b4a5303641
yap <yap@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2017-04-21 07:46:46 +0000

    yap-jinai:svncom
    git-svn-id: https://subversive.cims.nyu.edu/exact@6161 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

--- trunk/progs/robotics/complex-qt/robots/snowflake.py
+++ trunk/progs/robotics/complex-qt/robots/snowflake.py
@@ -1,6 +1,31 @@
-#  snowflake.rob
+#  snowflake.py
 #
-#  	This file describe a star-shaped robot.	
+#########################################
+#	The snowflake robot is decomposed into a central
+#		regular hexagon with side length 2,
+#	with 6 rectangles of dimensions 2xL  (the L's may be different
+#		for some rectangles). 
+#
+#	Assume L>2 so that the ``short sides'' of the rectangles
+#	have length 2.  The ``inner'' short side of each rectangle 
+#	abuts a side of the hexagon.  This suggests that the snowflake
+#	has 6 + (2x6) = 18 vertices.   However, we will need to 
+#	to introduce a vertex
+#	in the middle of the ``outer'' short side of each rectangle because
+#	of the requirement that T_i is nice.
+#	Thus, the snowflake is actually has 24 vertices.
+#		
+#	We design the snowflake so that only one irrational number,
+#			a = sqrt(3)=1.732
+#	is needed to describe the coordinates:
+#
+#
+#########################################
+#  GENERAL FORMAT FOR STAR SHAPE ROBOTS
+#########################################
+#  	This file uses a special format to describe star-shaped robots.
+#  	There is a python script to convert this snowflake.py file to a file
+#           "snowflake.rob"  which can be read by our C++ parser.
 #
 #	The general file format for a star-shaped is as follows:
 #
@@ -36,31 +61,50 @@
 #	We design the snowflake so that only one irrational number,
 #			a = sqrt(3)=1.732
 #	is needed to describe the coordinates:
+#########################################
 
+# N = 24
 24
 
+# Following are N vertices (one per line)
+###########################
+10	-1
 10	0
 10	1
+###########################
 a	1
+###########################
 2+a	1+2a
 2+a/2	3/2 + 2a
 2	2+2a
+###########################
 0	2
+###########################
 -2	2+2a
 -2-a/2	3/2 + 2a
 -2-a	1+2a
+###########################
 -a	1
+###########################
 -10	1
 -10	0
 -10	-1
+###########################
 -a	-1
+###########################
 -2-a	-1-2a
 -2-a/2	-3/2-2a
 -2	-2-2a
+###########################
 0	-2
+###########################
 2	-2-2a
 2+a/2	-3/2-2a
 2+a	-1-2a
+###########################
 a	-1
-10	-1
+###########################
 
+#########################################
+# END of file
+#########################################
