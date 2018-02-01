22cb2f9450b5cfee9c63d88455748d8966ec0d08
yap <yap@a02fd672-c32b-43fa-a32f-4ef1cc386a6b>
2018-02-01 09:46:30 +0000

    yap-jinai:svncom
    git-svn-id: https://subversive.cims.nyu.edu/exact@6995 a02fd672-c32b-43fa-a32f-4ef1cc386a6b

---
+++ trunk/progs/ieee/fpOverflow.cpp
@@ -0,0 +1,21 @@
+#include <cfenv>
+#include <iostream>
+
+int main() {
+    std::feclearexcept(FE_OVERFLOW);
+    std::feclearexcept(FE_UNDERFLOW);
+
+    double overflowing_var = 1000;
+    double underflowing_var = 0.01;
+
+    std::cout << "Overflow flag before: " << (bool)std::fetestexcept(FE_OVERFLOW) << std::endl;
+    std::cout << "Underflow flag before: " << (bool)std::fetestexcept(FE_UNDERFLOW) << std::endl;
+
+    for(int i = 0; i < 200; ++i) {
+        overflowing_var *= overflowing_var;
+        underflowing_var *= underflowing_var;
+    }
+
+    std::cout << "Overflow flag after: " << (bool)std::fetestexcept(FE_OVERFLOW) << std::endl;
+    std::cout << "Underflow flag after: " << (bool)std::fetestexcept(FE_UNDERFLOW) << std::endl;
+}
